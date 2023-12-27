a623036eb3514646d6b7d22d17018f86806abf05
Anh Tester <thaian.it15@gmail.com>
2023-12-27 17:17:30 +0700

    Handle data on Table

--- src/test/java/com/anhtester/Bai31_TestListener/pages/CustomerPage.java
+++ src/test/java/com/anhtester/Bai31_TestListener/pages/CustomerPage.java
@@ -3,8 +3,12 @@ package com.anhtester.Bai31_TestListener.pages;
 import com.anhtester.drivers.DriverManager;
 import com.anhtester.keywords.WebUI;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+
+import java.util.List;
 
 public class CustomerPage {
 
@@ -35,7 +39,7 @@ public class CustomerPage {
     private By inputSearchCountry = By.xpath("//div[@app-field-wrapper='country']//input[@aria-label='Search']");
     private By buttonSaveAndCreateContact = By.xpath("//button[normalize-space()='Save and create contact']");
     private By buttonSaveCustomer = By.xpath("//div[@id='profile-save-section']//button[normalize-space()='Save']");
-
+    private By pageTotal = By.xpath("//div[@id='DataTables_Table_0_info']");
 
     public void clickButtonAddNew() {
         WebUI.clickElement(buttonAddNewCustomer);
@@ -77,6 +81,63 @@ public class CustomerPage {
 
 
         return new ProfilePage();
+    }
+
+    public void searchCustomer2(String companyName) {
+        WebUI.setText(inputSearchCustomers, companyName);
+        WebUI.sleep(2);
+        WebUI.waitForPageLoaded();
+    }
+
+    public int checkPageTotal(int total) {
+        String pageTotalText = WebUI.getElementText(pageTotal);
+        System.out.println(pageTotalText);
+        //Showing 1 to 25 of 159 entries (filtered from 519 total entries)
+        String pageTotalNumber1[] = pageTotalText.split(" ");
+        //System.out.println(pageTotalNumber1);
+        String pageTotalNumber2 = pageTotalText.split(" ")[3];
+        System.out.println("Check Page Total: " + pageTotalNumber2);
+
+        Assert.assertEquals(Integer.parseInt(pageTotalNumber2), total, "The page total not match.");
+
+        return Integer.parseInt(pageTotalNumber2);
+
+    }
+
+    public int checkPageTotal() {
+        String pageTotalText = WebUI.getElementText(pageTotal);
+        System.out.println(pageTotalText);
+        //Showing 1 to 25 of 159 entries (filtered from 519 total entries)
+        String pageTotalNumber1[] = pageTotalText.split(" ");
+        //System.out.println(pageTotalNumber1);
+        String pageTotalNumber2 = pageTotalText.split(" ")[3];
+        System.out.println("Check Page Total: " + pageTotalNumber2);
+
+        return Integer.parseInt(pageTotalNumber2);
+
+    }
+
+    public void checkSearchTableByColumn(int column, String value) {
+
+        //Xác định số dòng của table sau khi search
+        List<WebElement> row = DriverManager.getDriver().findElements(By.xpath("//table//tbody/tr"));
+        int rowTotal = row.size(); //Lấy ra số dòng
+        System.out.println("Số dòng tìm thấy: " + rowTotal);
+
+        Assert.assertEquals(rowTotal, checkPageTotal(), "The page total not equals records total");
+
+        //Duyệt từng dòng
+        for (int i = 1; i <= rowTotal; i++) {
+            WebElement elementCheck = DriverManager.getDriver().findElement(By.xpath("//table//tbody/tr[" + i + "]/td[" + column + "]"));
+
+            JavascriptExecutor js = (JavascriptExecutor) DriverManager.getDriver();
+            js.executeScript("arguments[0].scrollIntoView(false);", elementCheck);
+
+            System.out.print(value + " - "); //Expected
+            System.out.println(elementCheck.getText()); //Actual
+            Assert.assertTrue(elementCheck.getText().toUpperCase().contains(value.toUpperCase()), "Dòng số " + i + " không chứa giá trị tìm kiếm.");
+        }
+
     }
 
 }
---
+++ src/test/java/com/anhtester/handle_components/Handle_Table_CustomerCRM.java
@@ -0,0 +1,26 @@
+package com.anhtester.handle_components;
+
+import com.anhtester.Bai31_TestListener.pages.CustomerPage;
+import com.anhtester.Bai31_TestListener.pages.DashboardPage;
+import com.anhtester.Bai31_TestListener.pages.LoginPage;
+import com.anhtester.common.BaseTest;
+import com.anhtester.constants.ConfigData;
+import org.testng.annotations.Test;
+
+public class Handle_Table_CustomerCRM extends BaseTest {
+
+    LoginPage loginPage;
+    DashboardPage dashboardPage;
+    CustomerPage customerPage;
+
+    @Test
+    public void testCheckDataOnTable(){
+        loginPage = new LoginPage();
+        dashboardPage = loginPage.loginCRM(ConfigData.EMAIL, ConfigData.PASSWORD);
+        customerPage = dashboardPage.clickMenuCustomers();
+        customerPage.searchCustomer2("Viettel A1"); //Tra ra 25 records /page
+        //customerPage.checkPageTotal(5);
+        customerPage.checkSearchTableByColumn(3, "Viettel A1"); //So sanh contains
+    }
+
+}
