c8b75056833caaa4c3d19020c29315bd842ab203
Anh Tester <thaian.it15@gmail.com>
2024-01-05 00:05:49 +0700

    Optimize code

--- src/main/java/com/anhtester/constants/ConfigData.java
+++ src/main/java/com/anhtester/constants/ConfigData.java
@@ -1,7 +1,17 @@
 package com.anhtester.constants;
 
+import com.anhtester.helpers.PropertiesHelper;
+
 public class ConfigData {
-    public static final String URL = "https://crm.anhtester.com";
-    public static final String EMAIL = "admin@example.com";
-    public static final String PASSWORD = "123456";
+    // public static String URL = "https://crm.anhtester.com/admin/authentication";
+    // public static String EMAIL = "admin@example.com";
+    // public static String PASSWORD = "123456";
+
+    static {
+        PropertiesHelper.loadAllFiles();
+    }
+
+    public static String URL = PropertiesHelper.getValue("url");
+    public static String EMAIL = PropertiesHelper.getValue("email");
+    public static String PASSWORD = PropertiesHelper.getValue("password");
 }
